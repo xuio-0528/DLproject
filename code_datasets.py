a560ad3be28d7d5003011999c9c52d4776427d66
xuio-0528 <sus5467@naver.com>
2023-11-23 16:19:30 +0900

    FIX: jsonl에 맞게 수정

--- code_datasets.py
+++ code_datasets.py
@@ -1,57 +1,33 @@
+import torch
+from torch.utils.data import Dataset
+import json
+from accelerate import Accelerator
 from datasets import load_dataset
-import json 
-from transformers import AutoTokenizer
-from tqdm import tqdm
 
-print('시작')
-data = load_dataset('codeparrot/apps')
-tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-1_5")
+class CodeDataset(Dataset):
+    def __init__(self, file_path):
+        self.examples = []
+        self.data = []
 
-train = []
-test = []
+        with open(file_path, "r") as f:
+            for line in f:
+                example = json.loads(line)
+                self.examples.append(example)
+        for example in self.examples:
+            self.data.append(
+                {
+                    "input" : self.data['description'],
+                    "output" : example["solution"],
+                }
+            )
 
-print('train 시작')
+    def map(self, func):
+        self.data = [func(item) for item in self.data]
+        
 
-for data_point in tqdm(data['train']):
-    try:
-      question = data_point['question']
-      if len(tokenizer(question).input_ids) > 512:
-          continue
-      inputs = json.loads(data_point['input_output'])['inputs']
-      output = json.loads(data_point['input_output'])['outputs']
-      cnt = 0
-      for solution in data_point['solutions']:
-          if cnt == 5:
-              break
-          if len(tokenizer(solution).input_ids) > 512:
-              continue
-          cnt +=1
-          train.append({'description' : question, 'solution' : solution, 'input' : inputs, 'output' : output})
-    except:
-        continue
-
-with open("apps_train.jsonl" , encoding= "utf-8",mode="w") as file:
-  for line in train:
-    file.write(json.dumps(line))
-    file.write('\n')
-
-print('test 시작')
-
-for data_point in tqdm(data['test']):
-    try:
-      question = data_point['question']
-      if len(tokenizer(question).input_ids) > 512:
-          continue
-      inputs = data_point['input_output']['inputs']
-      output = data_point['input_output']['outpus']
-      cnt = 0
-      test.append({'description' : question, 'solution' : "", 'input' : inputs, 'output' : output})
-    except:
-      continue
-
-with open("apps_test.jsonl" , encoding= "utf-8",mode="w") as file:
-    for line in test:
-        file.write(json.dumps(line))
-        file.write('\n')
+    def __len__(self):
+        return len(self.data)
     
-print('완료')
+    def __getitem__(self, idx):
+
+        return self.data[idx]
