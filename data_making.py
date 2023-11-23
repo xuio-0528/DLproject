9b457ceffe5757dcb801f5788243ba88a28002e3
xuio-0528 <sus5467@naver.com>
2023-11-23 16:19:51 +0900

    FIX: 최종 수정

--- data_making.py
+++ data_making.py
@@ -14,19 +14,19 @@ print('train 시작')
 
 for data_point in tqdm(data['train']):
     try:
-      question = data_point['question']
-      if len(tokenizer(question).input_ids) > 512:
-          continue
-      inputs = json.loads(data_point['input_output'])['inputs']
-      output = json.loads(data_point['input_output'])['outputs']
-      cnt = 0
-      for solution in data_point['solutions']:
-          if cnt == 5:
-              break
-          if len(tokenizer(solution).input_ids) > 512:
-              continue
-          cnt +=1
-          train.append({'description' : question, 'solution' : solution, 'input' : inputs, 'output' : output})
+        question = data_point['question']
+        if len(tokenizer(question).input_ids) > 512:
+            continue
+        inputs = json.loads(data_point['input_output'])['inputs']
+        output = json.loads(data_point['input_output'])['outputs']
+        cnt = 0
+        for solution in eval(data_point['solutions']):
+            if cnt == 5:
+                break
+            if len(tokenizer(solution).input_ids) > 512:
+                continue
+            cnt +=1
+            train.append({'description' : question, 'solution' : solution, 'input' : inputs, 'output' : output})
     except:
         continue
 
@@ -36,25 +36,19 @@ with open("./data/apps/apps_train.jsonl" , encoding= "utf-8",mode="w") as file:
         file.write('\n')
 
 print('test 시작')
-
+cnt=0
 for data_point in tqdm(data['test']):
     try:
-      question = data_point['question']
-      if len(tokenizer(question).input_ids) > 512:
-          continue
-      inputs = data_point['input_output']['inputs']
-      output = data_point['input_output']['outpus']
-      cnt = 0
-      for solution in data_point['solutions']:
-          if cnt == 5:
-              break
-          if len(tokenizer(solution).input_ids) > 512:
-              continue
-          cnt +=1
-          test.append({'description' : question, 'solution' : solution, 'input' : inputs, 'output' : output})
+        question = data_point['question']
+        if len(tokenizer(question).input_ids) > 512:
+            continue
+        inputs = json.loads(data_point['input_output'])['inputs']
+        output = json.loads(data_point['input_output'])['outputs']
+        test.append({'description' : question, 'solution' : solution, 'input' : inputs, 'output' : output})
+        cnt+=1
     except:
-      continue
-
+        continue  
+print(cnt)
 with open("./data/apps/apps_test.jsonl" , encoding= "utf-8",mode="w") as file:
     for line in test:
         file.write(json.dumps(line))
