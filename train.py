53bb283da8189a819ed6c81e33e45d84ce7c3454
xuio-0528 <sus5467@naver.com>
2023-11-24 10:52:21 +0900

    ADD:lora가 제대로 돌아가도록 수정

--- train.py
+++ train.py
@@ -1,13 +1,13 @@
 import os
 import sys
 from typing import List
+import bitsandbytes as bnb
 
 import numpy as np
 import fire
 import torch
 import transformers
 from datasets import load_dataset
-from code_datasets import CodeDataset
 
 """
 Unused imports:
@@ -22,10 +22,8 @@ from peft import (
     prepare_model_for_int8_training,
     set_peft_model_state_dict,
 )
-from transformers import LlamaForCausalLM, LlamaTokenizer, AutoTokenizer, CodeLlamaTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 from transformers import TrainerCallback, get_cosine_schedule_with_warmup
-
-from utils.prompter import Prompter
 
 
 # class LogCallback(TrainerCallback):
@@ -46,35 +44,35 @@ def set_seed(seed):
 def train(
     # model/data params
     base_model: str = "microsoft/phi-1_5",  # the only required argument
-    data_path: str = "./data/apps/",
+    data_path: str = "./",
     output_dir: str = "./",
     # training hyperparams
     batch_size: int = 1,
     micro_batch_size: int = 1,
     num_epochs: int = 200,
-    learning_rate: float = 3e-3,
+    learning_rate: float = 3e-5,
     cutoff_len: int = 1024,
     val_set_size: int = 2000,
     # lora hyperparams
-    lora_r: int = 8,
+    lora_r: int = 16,
     lora_alpha: int = 16,
     lora_dropout: float = 0.05,
     # lora_target_modules: List[str] = [
     #     "q_proj",
     #     "v_proj",
     # ],
-    lora_target_modules: List[str] = ['q_proj','k_proj','v_proj','o_proj'],
+    lora_target_modules: List[str] = ['Wqkv', 'out_proj'],
     # llm hyperparams
     train_on_inputs: bool = True,  # if False, masks out inputs in loss
     add_eos_token: bool = True,
     group_by_length: bool = False,  # faster, but produces an odd training loss curve
     # wandb params
-    wandb_project: str = "lora-llama2-code",
-    wandb_run_name: str = "1data50epoch",
-    wandb_watch: str = "false",  # options: false | gradients | all
-    wandb_log_model: str = "llama2",  # options: false | true
+    # wandb_project: str = "lora-llama2-code",
+    # wandb_run_name: str = "first",
+    # wandb_watch: str = "false",  # options: false | gradients | all
+    # wandb_log_model: str = "false",  # options: false | true
     resume_from_checkpoint: str = None,  # either training checkpoint or final adapter
-    prompt_template_name: str = "new_template",  # The prompt template to use, will default to alpaca.
+    prompt_template_name: str = "no_instruction",  # The prompt template to use, will default to alpaca.
     seed: int = 42,
 ):
     if int(os.environ.get("LOCAL_RANK", 0)) == 0:
@@ -96,10 +94,10 @@ def train(
             f"train_on_inputs: {train_on_inputs}\n"
             f"add_eos_token: {add_eos_token}\n"
             f"group_by_length: {group_by_length}\n"
-            f"wandb_project: {wandb_project}\n"
-            f"wandb_run_name: {wandb_run_name}\n"
-            f"wandb_watch: {wandb_watch}\n"
-            f"wandb_log_model: {wandb_log_model}\n"
+            # f"wandb_project: {wandb_project}\n"
+            # f"wandb_run_name: {wandb_run_name}\n"
+            # f"wandb_watch: {wandb_watch}\n"
+            # f"wandb_log_model: {wandb_log_model}\n"
             f"resume_from_checkpoint: {resume_from_checkpoint or False}\n"
             f"prompt template: {prompt_template_name}\n"
         )
@@ -109,7 +107,7 @@ def train(
     gradient_accumulation_steps = batch_size // micro_batch_size
     # seed 설정
     set_seed(seed)
-    
+
     prompter = Prompter(prompt_template_name)
 
     device_map = "auto"
@@ -120,25 +118,30 @@ def train(
         gradient_accumulation_steps = gradient_accumulation_steps // world_size
 
     # Check if parameter passed or if set within environ
-    use_wandb = len(wandb_project) > 0 or (
-        "WANDB_PROJECT" in os.environ and len(os.environ["WANDB_PROJECT"]) > 0
-    )
-    # Only overwrite environ if wandb param passed
-    if len(wandb_project) > 0:
-        os.environ["WANDB_PROJECT"] = wandb_project
-    if len(wandb_watch) > 0:
-        os.environ["WANDB_WATCH"] = wandb_watch
-    if len(wandb_log_model) > 0:
-        os.environ["WANDB_LOG_MODEL"] = wandb_log_model
+    # use_wandb = len(wandb_project) > 0 or (
+    #     "WANDB_PROJECT" in os.environ and len(os.environ["WANDB_PROJECT"]) > 0
+    # )
+    # # Only overwrite environ if wandb param passed
+    # if len(wandb_project) > 0:
+    #     os.environ["WANDB_PROJECT"] = wandb_project
+    # if len(wandb_watch) > 0:
+    #     os.environ["WANDB_WATCH"] = wandb_watch
+    # if len(wandb_log_model) > 0:
+    #     os.environ["WANDB_LOG_MODEL"] = wandb_log_model
 
     # test할 때는 wandb 사용하지 않음
     # use_wandb = None
-    
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.float16
+    )
     model = AutoModelForCausalLM.from_pretrained(
         base_model,
-        load_in_8bit=True,
-        torch_dtype=torch.float16,
-        device_map=device_map,
+        device_map={"":0},
+        trust_remote_code=True,
+        quantization_config=bnb_config
     )
     tokenizer = AutoTokenizer.from_pretrained(base_model)
     tokenizer.padding_side = "left"  # Allow batched inference
@@ -146,7 +149,7 @@ def train(
     tokenizer.pad_token_id = (
         0  # unk. we want this to be different from the eos token
     )
-    
+
     def tokenize(prompt, add_eos_token=True):
         # there's probably a way to do this with the tokenizer settings
         # but again, gotta move fast
@@ -194,8 +197,8 @@ def train(
             ]  # could be sped up, probably
         return tokenized_full_prompt
 
-    model = prepare_model_for_int8_training(model)
-    # model.resize_token_embeddings(len(tokenizer))
+    # model = prepare_model_for_int8_training(model)
+    model.resize_token_embeddings(len(tokenizer))
     config = LoraConfig(
         r=lora_r,
         lora_alpha=lora_alpha,
@@ -207,16 +210,13 @@ def train(
     model.enable_input_require_grads()
     model = get_peft_model(model, config)
 
-    if data_path.endswith(".json") or data_path.endswith(".jsonl") or 'baseline' in data_path:
-        # data = load_dataset("json", data_files=data_path)
-        train_data = DiffDataset(data_path+'apps_train.jsonl')
-        val_data = DiffDataset(data_path + 'apps_test.jsonl')
-    # else:
-    #     data = load_dataset(data_path)
+    # if data_path.endswith(".json") or data_path.endswith(".jsonl") or 'baseline' in data_path:
+    train_data = CodeDataset(data_path+'apps_train.jsonl')
+    val_data = CodeDataset(data_path + 'apps_test.jsonl')
 
     train_data.map(generate_and_tokenize_prompt)
     val_data.map(generate_and_tokenize_prompt)
-    
+
     if resume_from_checkpoint:
         # Check the available weights and load them
         checkpoint_name = os.path.join(
@@ -238,13 +238,13 @@ def train(
             print(f"Checkpoint {checkpoint_name} not found")
 
     model.print_trainable_parameters()  # Be more transparent about the % of trainable params.
-    
+
     if not ddp and torch.cuda.device_count() > 1:
         # keeps Trainer from trying its own DataParallelism when more than 1 gpu is available
         model.is_parallelizable = True
         model.model_parallel = True
-    
-    
+
+
     trainer = transformers.Trainer(
         model=model,
         train_dataset=train_data,
@@ -262,7 +262,7 @@ def train(
             logging_steps=1,
             logging_dir='./logs',
             optim="adamw_torch",
-            lr_scheduler_type = 'cosine', 
+            lr_scheduler_type = 'cosine',
             evaluation_strategy="steps" if val_set_size > 0 else "no",
             save_strategy="steps",
             eval_steps=50 if val_set_size > 0 else None,
@@ -272,8 +272,8 @@ def train(
             load_best_model_at_end=True if val_set_size > 0 else False,
             ddp_find_unused_parameters=False if ddp else None,
             group_by_length=group_by_length,
-            report_to="wandb" if use_wandb else None,
-            run_name=wandb_run_name if use_wandb else None,
+            # report_to="wandb" if use_wandb else None,
+            # run_name=wandb_run_name if use_wandb else None,
         ),
         data_collator=transformers.DataCollatorForSeq2Seq(
             tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=False
@@ -300,5 +300,4 @@ def train(
     )
 
 
-if __name__ == "__main__":
-    fire.Fire(train)
+fire.Fire(train)
